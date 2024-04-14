f40b41c23b2be429a40531f63709bd3c0663cf82
Kien Le <lekien.2004nek@gmail.com>
2024-04-14 11:10:01 +0700

    joooo

--- src/main/java/com/example/mygame/MenuGame.java
+++ src/main/java/com/example/mygame/MenuGame.java
@@ -6,8 +6,11 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.animation.ScaleTransition;
+import javafx.util.Duration;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -29,19 +32,19 @@ public class MenuGame extends Application {
         button1.getStyleClass().add("play-button"); // Thêm lớp CSS cho nút
 
         // Tải biểu tượng "play" từ tệp hình ảnh
-        Image playIconImage = new Image(new FileInputStream("E:/code/MyGame/src/main/java/image/play-button-arrowhead.png"));
+        Image playIconImage = new Image(new FileInputStream("E:/code/MyGame/src/main/java/image/play5.png"));
         ImageView playIconImageView = new ImageView(playIconImage);
 
         // Đặt biểu tượng "play" làm đồng hành của nút
         button1.setGraphic(playIconImageView);
-        playIconImageView.setFitWidth(32); // Đặt chiều rộng tối đa
-        playIconImageView.setFitHeight(32); // Đặt chiều cao tối đa
+        playIconImageView.setFitWidth(150); // Đặt chiều rộng tối đa
+        playIconImageView.setFitHeight(75); // Đặt chiều cao tối đa
 
         Button button2 = new Button("exit");
         Button button3 = new Button("help");
 
-        HBox hbox = new HBox(20, button1, button2, button3);
-        hbox.setAlignment(javafx.geometry.Pos.CENTER);
+        VBox vbox = new VBox(20, button1, button2, button3);
+        vbox.setAlignment(javafx.geometry.Pos.CENTER);
 
         StackPane layout1 = new StackPane();
 
@@ -49,12 +52,73 @@ public class MenuGame extends Application {
 
         scene1.getStylesheets().add("file:///E:/code/MyGame/src/main/java/style.css");
 
-        Image image1 = new Image("file:E:/code/MyGame/src/main/java/image/image1.png");
+        Image image1 = new Image("file:E:/code/MyGame/src/main/java/image/mazeimage.png");
         ImageView backgroundImage = new ImageView(image1);
         layout1.getChildren().add(backgroundImage);
         backgroundImage.fitWidthProperty().bind(primaryStage.widthProperty());
         backgroundImage.fitHeightProperty().bind(primaryStage.heightProperty());
-        layout1.getChildren().add(hbox);
+        layout1.getChildren().add(vbox);
+
+        // Hiệu ứng ấn nút
+        ScaleTransition scaleInButton1 = new ScaleTransition(Duration.millis(100), button1);
+        scaleInButton1.setFromX(1);
+        scaleInButton1.setFromY(1);
+        scaleInButton1.setToX(1.2);
+        scaleInButton1.setToY(1.2);
+
+        ScaleTransition scaleOutButton1 = new ScaleTransition(Duration.millis(100), button1);
+        scaleOutButton1.setFromX(1.2);
+        scaleOutButton1.setFromY(1.2);
+        scaleOutButton1.setToX(1);
+        scaleOutButton1.setToY(1);
+
+        ScaleTransition scaleInButton2 = new ScaleTransition(Duration.millis(100), button2);
+        scaleInButton2.setFromX(1);
+        scaleInButton2.setFromY(1);
+        scaleInButton2.setToX(1.2);
+        scaleInButton2.setToY(1.2);
+
+        ScaleTransition scaleOutButton2 = new ScaleTransition(Duration.millis(100), button2);
+        scaleOutButton2.setFromX(1.2);
+        scaleOutButton2.setFromY(1.2);
+        scaleOutButton2.setToX(1);
+        scaleOutButton2.setToY(1);
+
+        ScaleTransition scaleInButton3 = new ScaleTransition(Duration.millis(100), button3);
+        scaleInButton3.setFromX(1);
+        scaleInButton3.setFromY(1);
+        scaleInButton3.setToX(1.2);
+        scaleInButton3.setToY(1.2);
+
+        ScaleTransition scaleOutButton3 = new ScaleTransition(Duration.millis(100), button3);
+        scaleOutButton3.setFromX(1.2);
+        scaleOutButton3.setFromY(1.2);
+        scaleOutButton3.setToX(1);
+        scaleOutButton3.setToY(1);
+
+
+
+        button1.setOnMouseEntered(mouseEvent -> {
+            scaleInButton1.play();
+        });
+        button1.setOnMouseExited(mouseEvent -> {
+            scaleOutButton1.play();
+        });
+        button2.setOnMouseEntered(mouseEvent -> {
+            scaleInButton2.play();
+        });
+        button2.setOnMouseExited(mouseEvent -> {
+            scaleOutButton2.play();
+        });
+        button3.setOnMouseEntered(mouseEvent -> {
+            scaleInButton3.play();
+        });
+        button3.setOnMouseExited(mouseEvent -> {
+            scaleOutButton3.play();
+        });
+
+
+
 
         primaryStage.setScene(scene1);
         primaryStage.show();
