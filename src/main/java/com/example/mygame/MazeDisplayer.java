53bb495e0c4f80361e364aab31df2c3567bd2a3b
dfuc <69285923+dfuc-630@users.noreply.github.com>
2024-04-19 00:20:12 +0700

    Update MazeDisplayer.java

--- src/main/java/com/example/mygame/MazeDisplayer.java
+++ src/main/java/com/example/mygame/MazeDisplayer.java
@@ -3,9 +3,15 @@ package application;
 import java.util.Set;
 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+
 
 class MazeDisplayer
 {
@@ -32,20 +38,38 @@ class MazeDisplayer
 		setMazeData(mazeData );
 	}
 	
-	public Scene getSceneMaze(int x, int y) // x, y = toa do cua Scene 
+	public Scene getSceneMaze(int x, int y) //throws FileNotFoundException // x, y = toa do cua Scene 
 	{
 		for (int i = 0; i < mazeData.length; i++) {
 	        for (int j = 0; j < mazeData[i].length; j++) {
-	            Rectangle rect ; 
+//	            Rectangle rect ; 
 	            if (mazeData[i][j] == 1) 
 	            {
-	            	rect = new Rectangle(30, 30);
-	                rect.setFill(Color.BLACK);
+	            	Image name;
+					try {
+						name = new Image(new FileInputStream("C:\\Users\\MyPC\\Downloads/wall.jpg"));
+						ImageView wall = new ImageView(name) ;
+	                    wall.setFitWidth(30);
+	                    wall.setFitHeight(30);
+	                    root.add(wall, j, i);
+					} catch (FileNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 	            } else {
-	            	rect = new Rectangle(30, 30);
-	                rect.setFill(Color.WHITE);
+	                Image name;
+					try {
+						name = new Image(new FileInputStream("C:\\Users\\MyPC\\OneDrive\\Documents/path.jpg"));
+						ImageView path = new ImageView(name) ;
+	                    path.setFitWidth(30);
+	                    path.setFitHeight(30);
+	                    root.add(path, j, i);
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					}
+	            	
 	            }
-	            root.add(rect, j, i);
+//	            root.add(rect, j, i);
 	        }
 	    }
 		Scene scene = new Scene(root, 620, 620);
