5bacf30e6e745f8aff65a47c9d20cc695eaa172e
Phongbwb <167440096+Phongbwb@users.noreply.github.com>
2024-05-23 15:29:52 +0700

    Update MazeDrawer.java

--- src/main/java/com/example/mygame/MazeDrawer.java
+++ src/main/java/com/example/mygame/MazeDrawer.java
@@ -1,7 +1,6 @@
 package com.Game;
 
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.effect.BoxBlur;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 
@@ -12,7 +11,6 @@ public class MazeDrawer {
     private double cellSizeWidth;
     private double cellSizeHeight;
 
-    // Constructor
     public MazeDrawer(int[][] mazeData, double canvasWidth, double canvasHeight) {
         this.mazeData = mazeData;
         this.rows = mazeData.length;
@@ -21,29 +19,8 @@ public class MazeDrawer {
         this.cellSizeHeight = canvasHeight / rows;
     }
 
-    // Phương thức để trả về mazeData
     public int[][] getMazeData() {
         return this.mazeData;
-    }
-
-    public void drawMaze(GraphicsContext gc) {
-        // Calculate cell size
-        double cellSizeWidth = 750.0 / columns;
-        double cellSizeHeight = 750.0 / rows;
-        // Iterate through the maze data and draw walls and empty spaces
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                if (mazeData[i][j] == 0) {
-                    // Wall, draw a black rectangle
-                    gc.setFill(Color.BLACK);
-                    gc.fillRect(j * cellSizeWidth, i * cellSizeHeight, cellSizeWidth, cellSizeHeight);
-                }
-            }
-        }
-    }
-
-    public boolean isWall(int x, int y) {
-        return mazeData[y][x] == 0;
     }
 
     public double getCellSizeWidth() {
@@ -53,55 +30,57 @@ public class MazeDrawer {
     public double getCellSizeHeight() {
         return cellSizeHeight;
     }
+    public int getRows() {
+        return rows;
+    }
+    public int getColumns() {
+        return columns;
+    }
 
     public void highlightAreaAroundCharacter(GraphicsContext gc, double characterX, double characterY, double radius) {
-        // Clear the canvas
-        gc.clearRect(0, 0, 750, 750);
+        gc.clearRect(0, 0, 1000, 750);
 
-        // Load wall image
         Image wallImage = new Image(getClass().getResource("/Image/wall1.jpg").toString());
 
-        // Calculate the range of cells to draw around the character
         int characterColumn = (int) (characterX / cellSizeWidth);
         int characterRow = (int) (characterY / cellSizeHeight);
         int startColumn = Math.max(0, characterColumn - 1);
-        int endColumn = Math.min(columns , characterColumn + 3);
-        int startRow = Math.max(0, characterRow );
+        int endColumn = Math.min(columns, characterColumn + 2);
+        int startRow = Math.max(0, characterRow-1);
         int endRow = Math.min(rows - 1, characterRow + 2);
-        // Create a BoxBlur effect for the blur effect
-        BoxBlur blur = new BoxBlur();
-        blur.setWidth(5); // Adjust the width of the blur effect as needed
-        blur.setHeight(5); // Adjust the height of the blur effect as needed
 
-        // Iterate through the range of cells and draw walls or clear space
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-               
                 if (i >= startRow && i <= endRow && j >= startColumn && j <= endColumn) {
-                  
-                    // Draw wall or clear space around the character
                     if (mazeData[i][j] == 0) {
-                        // Draw wall
                         gc.drawImage(wallImage, j * cellSizeWidth, i * cellSizeHeight, cellSizeWidth, cellSizeHeight);
-
+                    } else {
+                        if ((i == startRow && j >= startColumn && j <= endColumn) ||
+                            (i == endRow && j >= startColumn && j <= endColumn) ||
+                            (j == endColumn && i >= startRow && i <= endRow)) {
+                            gc.setFill(Color.rgb(0, 0, 0, 0.5));
+                            gc.fillRect(j * cellSizeWidth, i * cellSizeHeight, cellSizeWidth, cellSizeHeight);
+                        } else {
+                            gc.clearRect(j * cellSizeWidth, i * cellSizeHeight, cellSizeWidth, cellSizeHeight);
+                        }
                     }
-                    else{
-                    if((i==startRow&& j >= startColumn && j <=endColumn)||(i==endRow&& j >= startColumn && j <=endColumn)||(j == endColumn&&i >= startRow && i <= endRow)){
-                        gc.setFill(Color.rgb(0, 0, 0, 0.5));
-                        gc.fillRect(j * cellSizeWidth, i * cellSizeHeight, cellSizeWidth, cellSizeHeight);
-                    }
-                     else {
-                        // Draw empty space (clear the area)
-                        gc.clearRect(j * cellSizeWidth, i * cellSizeHeight, cellSizeWidth, cellSizeHeight);
-                     }
-                    }
-                } 
-               
-                else {
-                    // Fill the area not close to the character with black color
+                } else {
                     gc.setFill(Color.BLACK);
                     gc.fillRect(j * cellSizeWidth, i * cellSizeHeight, cellSizeWidth, cellSizeHeight);
+                }
+            }
+        }
+    }
 
+    public void drawFullMap(GraphicsContext gc) {
+        Image wallImage = new Image(getClass().getResource("/Image/wall1.jpg").toString());
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (mazeData[i][j] == 0) {
+                    gc.drawImage(wallImage, j * cellSizeWidth, i * cellSizeHeight, cellSizeWidth, cellSizeHeight);
+                } else {
+                    gc.clearRect(j * cellSizeWidth, i * cellSizeHeight, cellSizeWidth, cellSizeHeight);
                 }
             }
         }
